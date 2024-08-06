387f7f0faca85a691e6d74d11c8bbd39e1e4c09e
Leti1225 <leticialet2512@outlook.com>
2024-08-06 20:01:02 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-"use client";
-
 import { useState } from "react";
 
 //SERVER COMPONENTS
 const Home = () => {
   const [] = useState();
-  return <h1 className="mb-0 bg-red-400 text-red-500">red text</h1>;
+  return <button>test</button>;
 };
 
 export default Home;
