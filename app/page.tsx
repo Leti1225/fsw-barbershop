f1e499f4466006ca5942abc494f9c05f6f46ec12
Leti1225 <leticialet2512@outlook.com>
2024-08-14 17:13:34 -0300

    refactor: add data functions to get bookings

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
 import { format } from "date-fns"
 import { ptBR } from "date-fns/locale"
+import { getConfirmedBookings } from "./_data/get-confirmed-booking"
 
 //SERVER COMPONENTS
 const Home = async () => {
@@ -23,26 +24,7 @@ const Home = async () => {
     },
   })
 
-  const confirmedBookings = session?.user
-    ? await db.booking.findMany({
-        where: {
-          userId: (session.user as any).id,
-          date: {
-            gte: new Date(),
-          },
-        },
-        include: {
-          service: {
-            include: {
-              barbershop: true,
-            },
-          },
-        },
-        orderBy: {
-          date: "asc",
-        },
-      })
-    : []
+  const confirmedBookings = await getConfirmedBookings()
 
   return (
     <div>
